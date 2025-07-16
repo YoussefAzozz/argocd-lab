2f1e1b30d94aa6f004545d7fba6f363dd1055363
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-17 00:32:03 +0300

    Update deployToK8s.groovy

--- vars/deployToK8s.groovy
+++ vars/deployToK8s.groovy
@@ -1,10 +1,10 @@
-def call(String tokenCredId, String serverCredId, String manifestPath = 'k8s/deployment.yml') {
+def call(String tokenCredId, String serverCredId, String manifestPath = 'k8s/deployment.yml', String namespace) {
     withCredentials([
         string(credentialsId: tokenCredId, variable: 'TOKEN'),
         string(credentialsId: serverCredId, variable: 'SERVER')
     ]) {
         sh """
-            kubectl apply -f ${manifestPath} --server="$SERVER" --token="$TOKEN" --insecure-skip-tls-verify=true
+            kubectl apply -f ${manifestPath} --server="$SERVER" --token="$TOKEN" --namespace ${namespace} --insecure-skip-tls-verify=true
         """
     }
 }
