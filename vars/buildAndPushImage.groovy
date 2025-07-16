e8e293768b8e7c23bcf9f4025da8cdc7629bc5b3
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-16 12:53:22 +0300

    Update buildAndPushImage.groovy

--- vars/buildAndPushImage.groovy
+++ vars/buildAndPushImage.groovy
@@ -2,6 +2,7 @@ def call(String imageName, String credentialsId) {
     withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         sh """
             docker build -t ${imageName}:${BUILD_NUMBER} .
+            trivy image --exit-code 1 --severity CRITICAL,HIGH ${imageName}:${BUILD_NUMBER}
             echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
             docker push ${imageName}:${BUILD_NUMBER}
         """
