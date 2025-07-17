349057e7085f271029ecfab4aa9a7171cd45b4a7
YoussefAzozz <94914939+YoussefAzozz@users.noreply.github.com>
2025-07-17 03:34:01 +0300

    Update update_deployment.groovy

--- vars/update_deployment.groovy
+++ vars/update_deployment.groovy
@@ -1,7 +1,7 @@
-def call(){
+def call(String image_environment){
   unstash 'deployment-yml'
   sh '''
-  sed -i "s|image:.*|image: '$DOCKER_IMAGE:${BUILD_NUMBER}'|g" k8s/deployment.yml
+  sed -i "s|image:.*|image: '$DOCKER_IMAGE:${image_environment}${BUILD_NUMBER}'|g" k8s/deployment.yml
   cat k8s/deployment.yml
   '''
 }
